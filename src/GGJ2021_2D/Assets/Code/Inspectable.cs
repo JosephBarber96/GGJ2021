cc8854adba2e8c066e954efdeddcef8689ee8bc0
Elliot Dewhurst <elliot+github@dewhurst.me>
2021-01-30 11:47:40 +0000

    Added inspectable prefab and scriptable object

---
+++ src/GGJ2021_2D/Assets/Code/Inspectable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inspectable : MonoBehaviour, IInteractable
+{
+    public InspectableInformation Information;
+    public void Interact(Player player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
