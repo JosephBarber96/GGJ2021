dfc3ec682ca5b96c9be0070dc9957008f2913876
Elliot Dewhurst <elliot+github@dewhurst.me>
2021-01-30 22:08:46 +0000

    Added vibrating point light

---
+++ src/GGJ2021_2D/Assets/Code/VibratingPointLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class VibratingPointLight : MonoBehaviour
+{
+    public Light2D Light;
+    public float VibrationAmount;
+    public float VibrationTime;
+
+    private float _targetMaxRadius;
+    private float _targetMinRadius;
+
+    void Start()
+    {
+        _targetMaxRadius = Light.pointLightOuterRadius + VibrationAmount;
+        _targetMinRadius = Light.pointLightOuterRadius + VibrationAmount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var t = Mathf.PingPong(Time.time * VibrationTime, VibrationAmount);
+        var e = Easing.Bounce.InOut(t);
+        Light.pointLightOuterRadius = e + _targetMinRadius;
+    }
+}
