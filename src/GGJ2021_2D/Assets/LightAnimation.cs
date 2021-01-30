df1be6b1f249153f293d9d0fe049ddb32958207d
johnydorman <Johndorman0@gmail.com>
2021-01-30 21:57:34 +0000

    launch pad lighting

---
+++ src/GGJ2021_2D/Assets/LightAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightAnimation : MonoBehaviour
+{
+
+    public bool Rotate = false;
+    public float RotationSpeed = 1.0f;
+
+    public float outerRadiusMax = 6;
+    public float outerRAdiusMin = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Rotate)
+        {
+            transform.Rotate(Vector3.forward, RotationSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
