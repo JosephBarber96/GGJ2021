0a8b31352bcfc3e6b99ac473eeb96640f4cfb8e7
Joseph Barber <Joseph_Barber@Hotmail.co.uk>
2021-01-31 10:18:45 +0000

    Create level change trigger, add into tutorial

---
+++ src/GGJ2021_2D/Assets/Code/LevelChangeTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChangeTrigger : MonoBehaviour
+{
+    public GameController.eScenes m_sceneToLoad;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player p = collision.gameObject.GetComponent<Player>();
+        if (p != null)
+        {
+            GameController.Instance.LoadScene(m_sceneToLoad);
+        }
+    }
+}
